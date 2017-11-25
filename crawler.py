1437013fe57e5e517fea8741dd80189f94e27aba
ororog <ororog@gmail.com>
2017-11-25 15:45:37 +0900

    change to class

--- crawler.py
+++ crawler.py
@@ -13,65 +13,73 @@ RIOT_API_KEY = os.getenv('RGAPI')
 # ororog: 200482207
 
 def main():
-  watcher = RiotWatcher(RIOT_API_KEY)
-  crawl(watcher, 200482207)
+  crawler = Crawler(RIOT_API_KEY)
+  crawler.crawl_match_by_id(200482207)
 
-def crawl(watcher, account_id, region='jp1'):
-  matchlist = watcher.match.matchlist_by_account(
-    region, account_id, begin_index=0, end_index=10)
+class Crawler:
+  def __init__(self, api_key):
+    self.__watcher = RiotWatcher(RIOT_API_KEY)
 
-  for match_ref in matchlist['matches']:
-    gameId = match_ref['gameId']
-    if not Match.objects.filter(gameId=gameId).first():
-      match_json = watcher.match.by_id(region, gameId)
-      match = Match.objects.create(**{k: match_json[k] for k in (
-        'seasonId', 'queueId', 'gameId', 'gameVersion',
-        'platformId', 'gameMode', 'mapId', 'gameType',
-        'gameDuration', 'gameCreation'
-      )})
+  def crawl_match_by_id(self,
+                        account_id,
+                        region='jp1',
+                        begin_index=0,
+                        end_index=3):
+    matchlist = self.__watcher.match.matchlist_by_account(
+      region, account_id, begin_index=begin_index, end_index=end_index)
 
-      participant_identities_json = match_json['participantIdentities']
-      for participant_identity_json in participant_identities_json:
-        participant_identity = ParticipantIdentity.objects.create(
-          match=match,
-          participantId=participant_identity_json['participantId'],
-        )
+    for match_ref in matchlist['matches']:
+      gameId = match_ref['gameId']
+      if not Match.objects.filter(gameId=gameId).first():
+        match_json = self.__watcher.match.by_id(region, gameId)
+        match = Match.objects.create(**{k: match_json[k] for k in (
+          'seasonId', 'queueId', 'gameId', 'gameVersion',
+          'platformId', 'gameMode', 'mapId', 'gameType',
+          'gameDuration', 'gameCreation'
+        )})
 
-        player_json = participant_identity_json['player']
-        args = {k: player_json[k] for k in (
-          'currentPlatformId', 'summonerName',
-          'matchHistoryUri', 'platformId', 'currentAccountId',
-          'profileIcon', 'summonerId', 'accountId'
-        )}
-        args['participantIdentity'] = participant_identity
-        player = Player.objects.create(**args)
+        participant_identities_json = match_json['participantIdentities']
+        for participant_identity_json in participant_identities_json:
+          participant_identity = ParticipantIdentity.objects.create(
+            match=match,
+            participantId=participant_identity_json['participantId'],
+          )
 
-      participants_json = match_json['participants']
-      for participant_json in participants_json:
-        participant = Participant.objects.create(
-          match=match,
-          participantId=participant_json['participantId'],
-          teamId=participant_json['teamId'],
-          championId=participant_json['championId'],
-        )
+          player_json = participant_identity_json['player']
+          args = {k: player_json[k] for k in (
+            'currentPlatformId', 'summonerName',
+            'matchHistoryUri', 'platformId', 'currentAccountId',
+            'profileIcon', 'summonerId', 'accountId'
+          )}
+          args['participantIdentity'] = participant_identity
+          player = Player.objects.create(**args)
 
-        participant_stats_json = participant_json['stats']
-        participants_stats = ParticipantStats.objects.create(
-          participant=participant,
-          win=(participant_stats_json['win'] == 'true'),
-        )
+        participants_json = match_json['participants']
+        for participant_json in participants_json:
+          participant = Participant.objects.create(
+            match=match,
+            participantId=participant_json['participantId'],
+            teamId=participant_json['teamId'],
+            championId=participant_json['championId'],
+          )
 
-        participant_timeline_json = participant_json['timeline']
-        participant_timeline = ParticipantTimeline.objects.create(
-          participant=participant,
-          participantId=participant_timeline_json['participantId'],
-          lane=participant_timeline_json['lane'],
-          role=participant_timeline_json['role'],
-        )
+          participant_stats_json = participant_json['stats']
+          participants_stats = ParticipantStats.objects.create(
+            participant=participant,
+            win=(participant_stats_json['win'] == 'true'),
+          )
 
-def to_snake(camel_case):
-  s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-  return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+          participant_timeline_json = participant_json['timeline']
+          participant_timeline = ParticipantTimeline.objects.create(
+            participant=participant,
+            participantId=participant_timeline_json['participantId'],
+            lane=participant_timeline_json['lane'],
+            role=participant_timeline_json['role'],
+          )
+
+  def __to_snake(camel_case):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 if __name__ == '__main__':
   main()
