9878b2c2b7d922c99ee8c9f34b7b5dc8fa998c2f
ororog <ororog@gmail.com>
2017-12-03 22:50:34 +0900

    Set env.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,22 @@
 import os
+import environ
 from .base import *
 
-SECRET_KEY = os.environ['SECRET_KEY']
+env = environ.Env()
+env_file = str(BASE_DIR.path('.env'))
+env.read_env(env_file)
+
+SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': env('DB_NAME'),
+    'USER': env('DB_USERNAME'),
+    'PASSWORD': env('DB_PASSWORD'),
+    'HOST': '',
+    'PORT': '',
+  }
+}
